0d2b0d736152b6c1ec120528d6ed5cf11bf0be5b
alain <alain@alain-laptop>
2019-04-02 13:43:32 +0200

    Thread aldatutak: 2 data daukate eraikitzailean

--- lab5/src/Execute.java
+++ lab5/src/Execute.java
@@ -4,43 +4,35 @@ public class Execute {
 	
 	public static void main(String[] args) {
 		
-//		Data DATA1 = new Data(Data.NONLOCKING,Data.NONLOCKING,"root","anderdbk","127.0.0.1");
-		Data DATA2 = new Data(Data.NONLOCKING,Data.NONLOCKING,"root","Alain-b-96","127.0.0.1");
+		Data DATA1 = new Data(mode,mode,"root","Alain-b-96","127.0.0.1");
+		Data DATA2 = new Data(mode,mode,"root","Alain-b-96","127.0.0.1");
 		
-//		ThreadA threadA = new ThreadA(DATA1);
-//		ThreadB threadB = new ThreadB(DATA1);
-//		ThreadC threadC = new ThreadC(DATA1);
-//		ThreadD threadD = new ThreadD(DATA1);
-//		ThreadE threadE = new ThreadE(DATA1);
-//		ThreadF threadF = new ThreadF(DATA1);
+		ThreadA threadA = new ThreadA(DATA1,DATA2);
+		ThreadB threadB = new ThreadB(DATA1,DATA2);
+		ThreadC threadC = new ThreadC(DATA1,DATA2);
+		ThreadD threadD = new ThreadD(DATA1,DATA2);
+		ThreadE threadE = new ThreadE(DATA1,DATA2);
+		ThreadF threadF = new ThreadF(DATA1,DATA2);
 		
-		ThreadA threadA2 = new ThreadA(DATA2);
-		ThreadB threadB2 = new ThreadB(DATA2);
-		ThreadC threadC2 = new ThreadC(DATA2);
-		ThreadD threadD2 = new ThreadD(DATA2);
-		ThreadE threadE2 = new ThreadE(DATA2);
-		ThreadF threadF2 = new ThreadF(DATA2);
+
 		
-//		DATA1.initializeSharedVariables();
-//		DATA1.showInitialValues();
+	
+		
+		DATA1.initializeSharedVariables();
+		DATA1.showInitialValues();
 		DATA2.initializeSharedVariables();
 		DATA2.showInitialValues();
 		
-//		new Thread(threadA).start();
-//		new Thread(threadB).start();
-//		new Thread(threadC).start();
-//		new Thread(threadD).start();
-//		new Thread(threadE).start();
-//		new Thread(threadF).start();
+		new Thread(threadA).start();
+		new Thread(threadB).start();
+		new Thread(threadC).start();
+		new Thread(threadD).start();
+		new Thread(threadE).start();
+		new Thread(threadF).start();
 		
-		new Thread(threadA2).start();
-		new Thread(threadB2).start();
-		new Thread(threadC2).start();
-		new Thread(threadD2).start();
-		new Thread(threadE2).start();
-		new Thread(threadF2).start();
+
 		
-//		DATA1.showFinalValues();
+		DATA1.showFinalValues();
 		DATA2.showFinalValues();
 	}
 }
--- lab5/src/ThreadE.java
+++ lab5/src/ThreadE.java
@@ -5,18 +5,17 @@ public class ThreadE extends Thread {
 	
 	String myName = "E";
 	Data myData;
+	Data myData2;
 	int myShareMode;
 	int myExclusiveMode;
+	int myShareMode2;
+	int myExclusiveMode2;
 	
 	
-//	public ThreadE(){
-//		myData = new Data(myData.NONLOCKING,myData.NONLOCKING);
-//		myShareMode = myData.NONLOCKING;
-//		myExclusiveMode = myData.NONLOCKING;
-//	}
 	
-	public ThreadE(Data data){
+	public ThreadE(Data data, Data data2){
 		myData = data;
+		myData2 = data2;
 		if (myData.LOCKING == Data.LOCKING){
 			myShareMode = myData.SHARE_LOCKING;
 			myExclusiveMode = myData.EXCLUSIVE_LOCKING;
@@ -24,6 +23,14 @@ public class ThreadE extends Thread {
 		else{
 			myShareMode = myData.NONLOCKING;
 			myExclusiveMode = myData.NONLOCKING;
+		}
+		if (myData2.LOCKING == Data.LOCKING){
+			myShareMode2 = myData2.SHARE_LOCKING;
+			myExclusiveMode2 = myData2.EXCLUSIVE_LOCKING;
+		}
+		else{
+			myShareMode2 = myData2.NONLOCKING;
+			myExclusiveMode2 = myData2.NONLOCKING;
 		}
 	}
 	
@@ -36,19 +43,25 @@ public class ThreadE extends Thread {
 		boolean committed;
 		boolean committed2;
 		myData.synchronyze();
+		myData2.synchronyze();
 		
-		while ( counter  < myData.NUMBER_OF_ITERATIONS || counter2 < myData.NUMBER_OF_ITERATIONS){
-			
-			committed = myData.procedureF(myName,counter,myShareMode,myExclusiveMode);
-			committed2= myData.procedureF(myName, counter2, myShareMode, myExclusiveMode);
-			if(committed){
-				counter = counter +1;
+		while ( counter  < myData.NUMBER_OF_ITERATIONS || counter2 < myData2.NUMBER_OF_ITERATIONS){
+			if (counter  < myData.NUMBER_OF_ITERATIONS) {
+				committed = myData.procedureF(myName,counter,myShareMode,myExclusiveMode);
+				if(committed){
+					counter = counter +1;
+				}
 			}
+			if (counter2  < myData2.NUMBER_OF_ITERATIONS) {
+				committed2= myData2.procedureF(myName, counter2, myShareMode2, myExclusiveMode2);
+
 			if(committed2){
 				counter2 = counter2 +1;
 			}
 		}
 		myData.finish();
+		myData2.finish();
 	}
 	
+	}
 }
